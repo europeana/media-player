b26db5d72946b9c41e33cc4d280090268855e4d8
Pieter van Leeuwen <p.vanleeuwen@noterik.nl>
2019-05-16 09:23:47 +0200

    Embedding player
    Included updated readme with some explanation about the options

--- README.md
+++ README.md
@@ -14,6 +14,46 @@ It's possible to use a different port by specifying this first like so:
 
 `EUPS_PORT=7788 yarn start:dev` to start with port 7788. Same for npm just include `EUPS_PORT=7788` at the beginning.
 
+Development mode will include a testdata component that allows for easy testing different sources
+
+### Test with production environment 
+
+`yarn start:prod` or `npm run start:prod`
+
+To test without development testdata
+
 ### Build for production
 
 `yarn build` or `npm run build`
+
+### Embed player
+
+To embed the player yourself first make sure to include the EuropeanaMediaPlayer
+
+```
+import EuropeanaMediaPlayer from './components/europeanamediaplayer';
+```
+
+The constructor of the EuropeanaMediaPlayer is
+
+```
+var player = new EuropeanaMediaPlayer(container, videoObject);
+```
+
+The constructor accepts two parameters
+
+- **container** is the DOM element in which to create the player.
+- **videoObject** is an object containing the video properties.
+
+The videoObject has the following required properties
+
+- **id** the identifier of the video object
+- **source** the url source of the video file
+- **duration** the duration of the video file
+
+Further the following optional properties can be specified
+
+- **width** the width of the video file (default is 640)
+- **height** the height of the video file (default is 480)
+- **mediatype** the mediatype of the video file (default is mp4)
+
---
+++ src/components/europeanamediaplayer/index.js
@@ -0,0 +1,57 @@
+import Glue from '../glue';
+import Player from '../player';
+import TestData from '../testdata';
+import Timeline from '../timeline';
+import AnnotationEditor from '../annotationeditor';
+
+import * as css from './index.css'
+
+const environment = process.env.NODE_ENV;
+
+var glue;
+var testData;
+var player;
+var timeline;
+var annotationeditor;
+
+function EuropeanaMediaPlayer(container, videoObj) {
+    $(container).append('<div id="eups-content-wrapper"><div id="eups-player"></div><div id="eups-timeline"></div></div><div id="eups-editor-wrapper"><div id="eups-annotationeditor"></div></div>');
+
+    glue = new Glue();
+
+    if (environment == "development") {
+        console.log("development environment detected");
+        testData = new TestData(document.getElementById('eups-content-wrapper'));
+        testData.init(glue);
+    }
+    
+    player = new Player(document.getElementById('eups-player'));
+    timeline = new Timeline(document.getElementById('eups-timeline'));
+    annotationeditor = new AnnotationEditor(document.getElementById('eups-annotationeditor'));
+
+    // init / render elements    
+    player.init(glue, videoObj);
+    timeline.init(glue);
+    annotationeditor.init(glue);
+
+    glue.listen("annotationeditor", "addannotation", this, storeAnnotation);
+    glue.listen("annotationeditor", "deleteannotation", this, storeAnnotation);
+    glue.listen("testItem", "clicked", this, getAnnotations);
+}
+
+function storeAnnotation(data) {
+    
+    //update current storage
+    localStorage.removeItem('annotations_'+player.getVideoId());
+    localStorage.setItem("annotations_"+player.getVideoId(), JSON.stringify(annotationeditor.getAnnotations()));
+}
+
+function getAnnotations(data) {
+    if (localStorage.getItem("annotations_"+player.getVideoId()) !== null) {
+        let annotations = JSON.parse(localStorage.getItem("annotations_"+player.getVideoId()));
+
+        glue.signal("main", "loadannotations", annotations);
+    }
+}
+
+module.exports = EuropeanaMediaPlayer;
