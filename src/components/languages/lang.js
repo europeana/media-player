c89cdc44017fae7e1aea177e441a0e5b27be3dc8
Pieter van Leeuwen <p.vanleeuwen@noterik.nl>
2019-11-27 16:45:59 +0100

    Subtitles handling
    * Subtitles button looking at text tracks to display all languages a subtitle is available for
    * On request of Hugo (Europeana) video plays/pauses when clicked on the video
    * decrease volume slider size on small containers

---
+++ src/components/languages/lang.js
@@ -0,0 +1,129 @@
+export default {
+    locales: [
+        {
+            name: 'Български',
+            code: 'bg',
+            iso: 'bg-BG'
+        },
+        {
+            name: 'Čeština',
+            code: 'cs',
+            iso: 'cs-CZ'
+        },
+        {
+            name: 'Dansk',
+            code: 'da',
+            iso: 'da-DK'
+        },
+        {
+            name: 'Deutsch',
+            code: 'de',
+            iso: 'de-DE'
+        },
+        {
+            name: 'Ελληνικά',
+            code: 'el',
+            iso: 'el-GR'
+        },
+        {
+            name: 'English',
+            code: 'en',
+            iso: 'en-GB'
+        },
+        {
+            name: 'Español',
+            code: 'es',
+            iso: 'es-ES'
+        },
+        {
+            name: 'Eesti',
+            code: 'et',
+            iso: 'et-EE'
+        },
+        {
+            name: 'Euskara',
+            code: 'eu',
+            iso: 'eu-ES'
+        },
+        {
+            name: 'Suomi',
+            code: 'fi',
+            iso: 'fi-FI'
+        },
+        {
+            name: 'Français',
+            code: 'fr',
+            iso: 'fr-FR'
+        },
+        {
+            name: 'Gaeilge',
+            code: 'ga',
+            iso: 'ga-IE'
+        },
+        {
+            name: 'Hrvatski',
+            code: 'hr',
+            iso: 'hr-HR'
+        },
+        {
+            name: 'Magyar',
+            code: 'hu',
+            iso: 'hu-HU'
+        },
+        {
+            name: 'Italiano',
+            code: 'it',
+            iso: 'it-IT'
+        },
+        {
+            name: 'Lietuvių',
+            code: 'lt',
+            iso: 'lt-LT'
+        },
+        {
+            name: 'Latviešu',
+            code: 'lv',
+            iso: 'lv-LV'
+        },
+        {
+            name: 'Malti',
+            code: 'mt',
+            iso: 'mt-MT'
+        },
+        {
+            name: 'Nederlands',
+            code: 'nl',
+            iso: 'nl-NL'
+        },
+        {
+            name: 'Polski',
+            code: 'pl',
+            iso: 'pl-PL'
+        },
+        {
+            name: 'Português',
+            code: 'pt',
+            iso: 'pt-PT'
+        },
+        {
+            name: 'Română',
+            code: 'ro',
+            iso: 'ro-RO'
+        },
+        {
+            name: 'Slovenčina',
+            code: 'sk',
+            iso: 'sk-SK'
+        },
+        {
+            name: 'Slovenščina',
+            code: 'sl',
+            iso: 'sl-SI'
+        },
+        {
+            name: 'Svenska',
+            code: 'sv',
+            iso: 'sv-SE'
+        }
+    ]
+}
