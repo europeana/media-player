d06f231eb6226c500f37dfca572949105fc7b6a0
Andy MacLean <andyjmaclean@gmail.com>
2020-03-17 15:57:47 +0100

    EA-1978 Feature tests: nightwatch setup (no selenium)

---
+++ spec-e2e/nightwatch-test.test.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'Load local fixture': (browser) => {
+    browser
+      .url('http://127.0.0.1:8081/spec/fixture-data/index.html')
+      .assert.visible('body')
+      .end()
+  },
+  'Search Europeana': (browser) => {
+    const parisText = `Paris as seen from the air . Showing the Place de L'etoile . 1 November 1928 | TopFoto.co.uk`;
+    browser
+      .url('https://www.europeana.eu/portal/en')
+      .assert.visible('input[name=q]')
+      .setValue('input[name=q]', 'paris')
+      .pause(1000)
+      .click('.search-submit')
+      .pause(1000)
+      .assert.containsText('ol.result-items li article .item-info h2 a', parisText)
+      .pause(1000)
+      .end()
+  },
+  after: (browser)=> {
+      browser.end()
+  }
+};
