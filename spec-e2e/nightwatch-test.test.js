6e652d9261ccdaf9a5cb68c739664824777044ae
Jonathan Blok <jblok@beeldengeluid.nl>
2020-03-20 13:11:31 +0100

    Adding test to test Play functionality.

--- spec-e2e/nightwatch-test.test.js
+++ spec-e2e/nightwatch-test.test.js
@@ -1,22 +1,15 @@
-module.exports = {
-  'Load local fixture': (browser) => {
+module.exports = { 
+  'Clicking play': (browser) => {
     browser
       .url('http://127.0.0.1:8081/spec/fixture-data/index.html')
+      .waitForElementVisible('.button-play', 5000)
+      .waitForElementVisible('.canvas-time', 5000)
+      .assert.containsText('.canvas-time', '00:01')
+      .click('.button-play')
       .pause(2000)
-      .assert.visible('div[class=eups-player]')
-      .end()
-  },
-  'Search Europeana': (browser) => {
-    const parisText = `Paris as seen from the air . Showing the Place de L'etoile . 1 November 1928 | TopFoto.co.uk`;
-    browser
-      .url('https://www.europeana.eu/portal/en')
-      .assert.visible('input[name=q]')
-      .setValue('input[name=q]', 'paris')
-      .pause(1000)
-      .click('.search-submit')
-      .pause(1000)
-      .assert.containsText('ol.result-items li article .item-info h2 a', parisText)
-      .pause(1000)
+      .assert.attributeContains('.button-play', 'title', 'Pause')
+      .assert.not.containsText('.canvas-time', '00:01')
+      .getValue('.canvas-time')
       .end()
   },
   after: (browser)=> {
