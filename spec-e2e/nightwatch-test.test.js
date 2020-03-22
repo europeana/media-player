2c565a06c557c8ca78f8955a689cd50fc61dc8ab
Jonathan Blok <jblok@beeldengeluid.nl>
2020-03-22 11:40:07 +0100

    Adding test case for pausing a video.

--- spec-e2e/nightwatch-test.test.js
+++ spec-e2e/nightwatch-test.test.js
@@ -6,10 +6,19 @@ module.exports = {
       .waitForElementVisible('.canvas-time', 5000)
       .assert.containsText('.canvas-time', '00:01')
       .click('.button-play')
-      .pause(2000)
+      .waitForElementVisible('.pause', 5000)
       .assert.attributeContains('.button-play', 'title', 'Pause')
       .assert.not.containsText('.canvas-time', '00:01')
-      .getValue('.canvas-time')
+      .end()
+  },
+  'Clicking play and then pause': (browser) => {
+    browser
+      .url('http://127.0.0.1:8081/spec/fixture-data/index.html')
+      .waitForElementVisible('.button-play', 5000)
+      .click('.button-play')
+      .waitForElementVisible('.pause', 5000)
+      .click('.button-play')
+      .waitForElementVisible('.play', 5000)
       .end()
   },
   after: (browser)=> {
