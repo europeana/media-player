13d0e8783098ff3275668490ce45ee949cd35ab3
Jonathan Blok <jblok@beeldengeluid.nl>
2020-03-25 15:55:40 +0100

    Adding assertion via callback to maximise and minimising test.

--- spec-e2e/nightwatch-test.test.js
+++ spec-e2e/nightwatch-test.test.js
@@ -1,5 +1,5 @@
 module.exports = { 
-  'Clicking play': (browser) => {
+  'Clicking play': (browser) => { 
     browser
       .url('http://127.0.0.1:8081/spec/fixture-data/index.html')
       .waitForElementVisible('.button-play', 5000)
@@ -42,21 +42,33 @@ module.exports = {
       .assert.containsText('.canvas-time', '00:49')
       .assert.attributeContains('.ui-slider-handle', 'style', 'left: 56')
       .end()
-  },
+  }, 
   'Maximising & minimising the player': (browser) => {
     browser
       .url('http://127.0.0.1:8081/spec/fixture-data/index.html')
       .waitForElementVisible('.av-icon-fullscreen', 5000)
+      .getElementSize('.canvas-container', function (result) {
+        this.assert.ok(result.value.height < '500', 'Checking to see if the height of the element is smaller than 500px before clicking fullscreen.')
+      })    
       .click('.av-icon-fullscreen')
+      .getElementSize('.canvas-container', function (result) {
+        this.assert.ok(result.value.width > '1000', 'Checking to see if the width of the element is greater than 1000px after clicking fullscreen.');
+        this.assert.ok(result.value.height > '500', 'Checking to see if the height of the element is greater than 500px after clicking fullscreen.');
+      })      
       .waitForElementVisible('.play', 5000)
       .click('.av-icon-fullscreen')
+      .getElementSize('.canvas-container', function (result) {
+        this.assert.ok(result.value.height < '500', 'Checking to see if the height of the element is smaller than 500px after leaving fullscreen.')
+      })
       .waitForElementVisible('.play', 5000)
       .end()
-  },
+    
+    },
   'Volume controls': (browser) => {
     browser
       .url('http://127.0.0.1:8081/spec/fixture-data/index.html')
       .waitForElementVisible('.volume-slider', 5000)
+      .assert.attributeEquals('.volume-mute', 'title','Mute')
       .moveToElement('.volume-slider', 0, 4)
       .mouseButtonClick(0)
       .assert.attributeEquals('.volume-mute', 'title','Unmute')
