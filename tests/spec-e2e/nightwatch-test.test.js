217451ac349e20a611a9ac6bc417ffe5e4355128
andyjmaclean <andyjmaclean@gmail.com>
2020-04-06 11:39:25 +0200

    EA-1978 Added headless configuration / run permutation commands / runner refactor / README

--- tests/spec-e2e/nightwatch-test.test.js
+++ tests/spec-e2e/nightwatch-test.test.js
@@ -1,43 +1,50 @@
 const target_url = 'http://127.0.0.1:8081/tests/fixture-data/index.html';
+const selTime = '.canvas-time';
+const selTimeline = '.canvas-timeline-container';
+const videoEndTime = '02:42';
+const videoMidTime = '01:21';
+
 module.exports = {
    tags: ['skip-firefox'],
    beforeEach: (browser) => {
      browser.url(target_url)
      .waitForElementVisible('.button-play')
-     .waitForElementVisible('.canvas-time');
+     .waitForElementVisible(selTime);
    },
-   /*
   'Using slider to scrub video': (browser) => {
+    const extraWaitTime = 1;
     browser
       .assert.attributeContains('.ui-slider-handle', 'style', 'left: 0')
-      .moveToElement('.ui-corner-all', 0, 0)
+      .moveToElement(selTimeline, 0, 2)
       .mouseButtonDown(0)
-      .moveToElement('.ui-corner-all', 200, 0)
-      .mouseButtonUp(0)
-      .assert.containsText('.canvas-time', '00:24')
-      .moveToElement('.ui-corner-all', 200, 0)
-      .mouseButtonDown(0)
-      .moveToElement('.ui-corner-all', 760, 0)
-      .mouseButtonUp(0)
-      .getText('.canvas-time', function(result) {
-        console.log('.canvas-time = actual = ' + result);
+      .getElementSize(selTimeline, function (result) {
+        this.moveToElement(selTimeline, result.value.width, result.value.height / 2);
       })
-      .assert.containsText('.canvas-time', '01:29')
+      .mouseButtonUp(0)
+      .pause(extraWaitTime)
+      .assert.containsText(selTime, videoEndTime)
+      .mouseButtonDown(0)
+      .getElementSize(selTimeline, function (result) {
+        this.moveToElement(selTimeline, (result.value.width / 2) -1, result.value.height / 2);
+      })
+      .mouseButtonUp(0)
+      .pause(extraWaitTime)
+      .getText(selTime, function(result) {
+        let initialValue = result.value;
+        console.log(`${selTime} actual value = ${initialValue}`);
+      })
+      .assert.containsText(selTime, videoMidTime)
       .end()
   },
-  */
   'Volume controls': (browser) => {
     browser
       .assert.attributeEquals('.volume-mute', 'title','Mute')
-      .moveToElement('.volume-slider', 0, 4)
+      .moveToElement('.volume-slider', 0, 2)
       .mouseButtonClick(0)
       .assert.attributeEquals('.volume-mute', 'title','Unmute')
       .moveToElement('.volume-slider', 70, 2)
       .mouseButtonClick(0)
       .assert.attributeEquals('.volume-mute', 'title','Mute')
       .end()
-  },
-  after: (browser) => {
-    browser.end();
   }
 };
