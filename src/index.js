a53e907de22b60b1ce8d906595c2b354a83e0248
Pieter van Leeuwen <p.vanleeuwen@noterik.nl>
2020-04-16 16:06:52 +0200

    Fixed a linting issue

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ export const init = (container, videoObj, options) => {
 
   //Initialize player
   player.init(videoObj, editorurl, language);
-} ;
+};
 
 export const uuidv4 = () => {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
