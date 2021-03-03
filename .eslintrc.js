0b7a0b12c581ff6012bcfd698f81a7fca59f62bb
mirjamverloop <30619959+mirjamverloop@users.noreply.github.com>
2021-03-03 11:01:08 +0100

    Merge branch 'develop' into feat/EC-5067-update-e2e-tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,31 +13,73 @@ module.exports = {
     'eslint:recommended'
   ],
   rules: {
+    'array-bracket-newline': ['error', 'consistent'],
+    'array-bracket-spacing': ['error', 'never'],
+    'array-element-newline': ['error', 'consistent'],
     'arrow-spacing': 'error',
     'block-spacing': ['error', 'always'],
     'brace-style': ['error', '1tbs', { 'allowSingleLine': false }],
     'camelcase': ['error', { 'ignoreDestructuring': false }],
     'comma-dangle': ['error', 'never'],
     'comma-spacing': ['error', { 'before': false, 'after': true }],
+    'comma-style': ['error', 'last'],
     'computed-property-spacing': ['error', 'never'],
     'eol-last': ['error', 'always'],
     'eqeqeq': ['error', 'always'],
+    'func-call-spacing': ['error', 'never'],
+    'func-style': ['error', 'declaration', { 'allowArrowFunctions': true }],
+    // Disabled pending CodeClimate support of eslint ^6.2.0
+    // 'function-call-argument-newline': ['error', 'consistent'],
     'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'key-spacing': ['error', { 'afterColon': true }],
     'keyword-spacing': ['error', { 'before': true, 'after': true }],
     'linebreak-style': ['error', 'unix'],
+    // Disabled pending manual resolution of violations
+    // 'max-len': ['error', { 'code': 120 }],
+    'no-array-constructor': 'error',
+    'no-bitwise': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-lonely-if': 'error',
+    'no-mixed-operators': 'error',
+    'no-multi-assign': 'error',
+    'no-multiple-empty-lines': ['error', { 'max': 1 }],
+    'no-negated-condition': 'error',
+    'no-nested-ternary': 'error',
+    'no-new-object': 'error',
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
+    'no-tabs': 'error',
     'no-trailing-spaces': 'error',
+    'no-unneeded-ternary': 'error',
     'no-var': 'error',
     'no-whitespace-before-property': 'error',
+    'nonblock-statement-body-position': ['error', 'beside'],
+    'object-curly-newline': ['error', { 'consistent': true }],
     'object-curly-spacing': ['error', 'always'],
     'object-shorthand': ['error', 'always'],
+    'padded-blocks': ['error', 'never'],
     'prefer-arrow-callback': 'error',
     'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
     'semi-spacing': ['error'],
+    'semi-style': ['error', 'last'],
+    'semi': ['error', 'always'],
+    // Disabled pending manual resolution of violations
+    // 'sort-keys': 'error',
     'space-before-blocks': ['error', 'always'],
     'space-before-function-paren': ['error', 'never'],
-    'space-in-parens': ['error', 'never']
-  }
+    'space-in-parens': ['error', 'never'],
+    'space-infix-ops': ['error', { 'int32Hint': false }],
+    'space-unary-ops': 'error',
+    'spaced-comment': ['error', 'always'],
+    'switch-colon-spacing': 'error'
+  },
+  'overrides': [
+    {
+      'files': ['lang/*.js'],
+      'rules': {
+        'quotes': ['error', 'double'],
+      }
+    }
+  ]
 }
--- src/index.js
+++ src/index.js
@@ -9,28 +9,30 @@ let player;
 let language = '';
 
 export const init = (container, videoObj, options) => {
-  //Create unique player id
+  // Create unique player id
   let playerId = uuidv4();
 
-  //Append player to container with the unique id
+  // Append player to container with the unique id
   $(container).append('<section id="eups-player-' + playerId + '" class="eups-player"></section>');
 
-  //Set options
+  // Set options
   editorurl = options.editor || editorurl;
   language = options.language || language;
 
-  //Create player
+  // Create player
   player = new Player(document.getElementById('eups-player-' + playerId));
 
-  //Initialize player
+  // Initialize player
   player.init(videoObj, editorurl, language);
 };
 
+/* eslint-disable */
 export const uuidv4 = () => {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
   );
 };
+/* eslint-enable */
 
 export default class EuropeanaMediaPlayer {
   constructor(container, videoObj, options) {
